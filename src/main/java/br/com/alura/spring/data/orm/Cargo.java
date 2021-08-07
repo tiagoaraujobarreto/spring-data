6e5a7cd8741994f96d7a23962a3bd09b3dd21663
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-08-06 22:08:20 -0300

    Gerando o banco de dados

---
+++ src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -0,0 +1,31 @@
+package br.com.alura.spring.data.orm;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cargos")
+public class Cargo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	private String descricao;
+	
+}
