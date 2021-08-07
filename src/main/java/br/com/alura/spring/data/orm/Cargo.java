15e0e1511232e500d6bae4552f59810b93e476f9
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-08-07 00:50:24 -0300

    Inserindo valores na tabela Cargo

--- src/main/java/br/com/alura/spring/data/orm/Cargo.java
+++ src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -1,4 +1,4 @@
-package orm;
+package br.com.alura.spring.data.orm;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,23 +9,26 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "cargos")
 public class Cargo {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+	private String descricao;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	private String descricao;
-	
+
 }
--- src/main/java/repository/CargoRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CargoRepository extends CrudRepository {
-
-}
